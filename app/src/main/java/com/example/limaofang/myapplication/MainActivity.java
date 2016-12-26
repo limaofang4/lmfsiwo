1d04c29502a6fdc6dcd0e98e40921eab782a4b09
李茂方 <limaofang@limaofangdeMacBook-Air.local>
2016-12-27 00:48:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/limaofang/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.limaofang.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Log.d(TAG, "onCreate: ");
+       // Log.d(TAG, "onCreate: ");
+//     Butt
+
+
+    }
+}
